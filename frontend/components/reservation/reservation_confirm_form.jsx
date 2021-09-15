43e7c01b8ed7fce2e7eb49ff10ea8498449c85ca
Aiden Chang <skylineciel@gmail.com>
2021-09-14 22:03:58 -0400

    edit reservation form component

--- frontend/components/reservation/reservation_confirm_form.jsx
+++ frontend/components/reservation/reservation_confirm_form.jsx
@@ -1,81 +1,87 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { createReservation } from '../../actions/reservation_actions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser as farUser } from '@fortawesome/free-regular-svg-icons'
 
 class ReservationConfirmForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.updatePhoneNumber = this.updatePhoneNumber.bind(this);
-        this.updateSpecialRequest = this.updateSpecialRequest.bind(this);
-        if (!this.props.location.state) {
-            this.props.history.push('/');
-            window.location.reload();
-        }
+  constructor(props) {
+    super(props);
 
-        const { party_size, date, time, restaurant, currentUser } = this.props.location.state
-        this.state = {
-            party_size,
-            date,
-            time,
-            user_id: currentUser.id,
-            restaurant_id: restaurant.id,
-            email: currentUser.email,
-            phone_number: 0,
-            special_request: ""
-        }
+    if (!this.props.location.state) {
+      this.props.history.push('/');
+      window.location.reload();
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.createReservation(this.state)
-            .then(res => this.props.history.push(`/booking/view/${res.reservation.id}`));
-    }
-    updatePhoneNumber(e) {
-        e.preventDefault();
-        this.setState({phone_number: parseInt(e.target.value)})
-    }
-    updateSpecialRequest(e) {
-        e.preventDefault();
-        this.setState({special_request: e.target.value})
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.update = this.update.bind(this);
 
-    render () {
-        const { party_size, date, time, restaurant, currentUser } = this.props.location.state
-        return (
-            <div className="reservation-confirm-form">
-                <h1>You're almost done!</h1>
-                <div className="reservation-confirm-form-overview">
-                    <br />
-                    <img src="" />
-                    <h2>{restaurant.name}</h2>
-                    <br />
-                    <p>{date}</p>
-                    <p>{time}</p>
-                    <p>{party_size} people</p>
-                    <br />
-                </div>
-                <form onSubmit={this.handleSubmit} className="reservation-confirm-form-info">
-                    <h3>Diner details</h3>
-                    <p>{currentUser.first_name} {currentUser.last_name} (<span>Not {currentUser.first_name}?</span>)</p>
-                    <input type="text" placeholder="Phone number" onChange={this.updatePhoneNumber}/>
-                    <input type="email" value={currentUser.email} readOnly/>
-                    <br />
-                    <textarea placeholder="Add a special request (optional)" onChange={this.updateSpecialRequest}/>
-                    <br />
-                    <input type="submit" value="Complete Reservation"/>
-                    <div className="reservation-confirm-form-terms">
-                        <p>By clicking “Complete reservation” you agree to the <span>ObeyTime Terms of Use</span> and <span>Privacy Policy</span>. </p>
-                    </div>
-                </form>
-                <div className="reservation-confirm-form-note">
-                    <h3>What to know before you go</h3>
-                    <h4>Important dining information</h4>
-                    <p>We may contact you about this reservation, so please ensure your email and phone number are up to date.</p>
-                </div>
-            </div>
-        )
+    const { party_size, date, time, restaurant, currentUser } = this.props.location.state;
+    this.state = {
+      party_size,
+      date,
+      time,
+      user_id: currentUser.id,
+      restaurant_id: restaurant.id,
+      email: currentUser.email,
+      phone_number: 1234567890,
+      special_request: ""
     }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createReservation(this.state)
+      .then(res => this.props.history.push(`/booking/view/${res.reservation.id}`));
+  }
+  
+  update(field) {
+    return e => this.setState({
+      [field]: e.target.value
+    });
+  }
+
+  render () {
+    const { party_size, date, time, restaurant, currentUser } = this.props.location.state;
+      
+    return (
+      <div className="reservation-confirm-form">
+        <h1>You're almost done!</h1>
+        <br />
+
+        <div className="reservation-confirm-form-overview">
+          <img src="" />
+          <h2>{restaurant.name}</h2>
+          <br />
+          
+          <p>{date}</p>
+          <p>{time}</p>
+          <p><FontAwesomeIcon className="profile" icon={farUser} /> {party_size} people</p>
+          <br />  
+        </div>
+        <form onSubmit={this.handleSubmit} className="reservation-confirm-form-info">
+          <h3>Diner details</h3>
+          <p>{currentUser.first_name} {currentUser.last_name} (<span>Not {currentUser.first_name}?</span>)</p>
+              
+          <input type="text" placeholder="Phone number" onChange={this.update('phone_number')}/>
+          <input type="email" value={currentUser.email} readOnly/>
+          <br />
+          <textarea placeholder="Add a special request (optional)" onChange={this.update('special_request')}/>
+          <br />
+
+          <input type="submit" value="Complete Reservation"/>
+          <div className="reservation-confirm-form-terms">
+            <p>By clicking “Complete reservation” you agree to the <span>ObeyTime Terms of Use</span> and <span>Privacy Policy</span>. </p>
+          </div>
+        </form>
+        <div className="reservation-confirm-form-note">
+          <h3>What to know before you go</h3>
+          <h4>Important dining information</h4>
+          <p>We may contact you about this reservation, so please ensure your email and phone number are up to date.</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
--- frontend/components/reservation/reservation_form.jsx
+++ frontend/components/reservation/reservation_form.jsx
@@ -2,103 +2,99 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class ReservationForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.updateTime = this.updateTime.bind(this);
-        this.updatePartySize = this.updatePartySize.bind(this);
-        this.updateDate = this.updateDate.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-        this.state = {
-            party_size: 2,
-            date: '2021-12-24',
-            time: '18:00',
-            displayTime: false
-        }
-    }
+  constructor(props) {
+      super(props);
 
-    updateTime (e) {
-        this.setState({ ...this.state, time: e.target.value })
-    }
+      this.update = this.update.bind(this);
+      this.handleClick = this.handleClick.bind(this);
 
-    updatePartySize(e) {
-        this.setState({ ...this.state, party_size: parseInt(e.target.value) })
-    }
+      this.state = {
+        party_size: 2,
+        date: '2021-12-24',
+        time: '18:00',
+        displayTime: false
+      }
+  }
 
-    updateDate (e) {
-        this.setState({ ...this.state, date: e.target.value })
-    }
+  update(field) {
+    return e => this.setState({
+      [field]: e.target.value
+    });
+  }
 
-    handleClick (e) {
-        e.preventDefault();
-        if (!this.props.currentUser) {
-            this.props.openModal('login');
-        }
-        else {
-            this.setState({
-                displayTime: true
-            })
-        }
+  handleClick (e) {
+    e.preventDefault();
+    if (!this.props.currentUser) {
+      this.props.openModal('login');
+    } else {
+      this.setState({
+        displayTime: true
+      })
     }
+  }
 
-    render () {
-        return (
-            <div>
-                <div className="reservation-form">
-                    <form className="reservation-form-box">
-                        <h3>Make a reservation</h3>
-                        <hr />
-                        <br/>
-                        <div className="reservation-form-input">
-                            <label>Party Size
-                                <br />
-                                <select className="reservation-input party" name="party_size" defaultValue="2" onChange={this.updatePartySize} >
-                                    <option value="1">1 person</option>
-                                    <option value="2">2 people</option>
-                                    <option value="3">3 people</option>
-                                    <option value="4">4 people</option>
-                                    <option value="5">5 people</option>
-                                    <option value="6">6 people</option>
-                                    <option value="7">7 people</option>
-                                    <option value="8">8 people</option>
-                                    <option value="9">9 people</option>
-                                    <option value="10">10 people</option>
-                                </select>
-                            </label>
-                            <br />
-                            <label>Time
-                                <br />
-                                <input className="search-input time" type="time" defaultValue={this.state.time} step="1800" onChange={this.updateTime}/>
-                            </label>
-                            <br />
-                            <label>Date
-                                <br />
-                                <input className="reservation-input date" name="date" defaultValue={this.state.date} type="date" onChange={this.updateDate} />
-                            </label>
-                            <br />
+  render () {
+    return (
+      <div>
+        <div className="reservation-form">
+          <form className="reservation-form-box">
+            <h3>Make a reservation</h3>
+            <hr />
+            <br/>
+                        
+            <div className="reservation-form-input">
+                <label>Party Size
+                  <br />
+                  <select className="reservation-input party" defaultValue="2" onChange={this.update('party_size')} >
+                    <option value="1">1 person</option>
+                    <option value="2">2 people</option>
+                    <option value="3">3 people</option>
+                    <option value="4">4 people</option>
+                    <option value="5">5 people</option>
+                    <option value="6">6 people</option>
+                    <option value="7">7 people</option>
+                    <option value="8">8 people</option>
+                    <option value="9">9 people</option>
+                    <option value="10">10 people</option>
+                  </select>
+                </label>
+                <br />
+
+                <label>Time
+                  <br />
+                  <input className="reservation-input time" type="time" value={this.state.time} onChange={this.update('time')}/>
+                </label>
+                <br />
+                          
+                <label>Date
+                  <br />
+                  <input className="reservation-input date" value={this.state.date} type="date" onChange={this.update('date')} />
+                </label>
+                <br />
                             
-                            { this.state.displayTime ? (
-                                <Link to={{
-                                    pathname: '/booking/details',
-                                    state: {
-                                        party_size: this.state.party_size,
-                                        date: this.state.date,
-                                        time: this.state.time,
-                                        restaurant: this.props.restaurant,
-                                        currentUser: this.props.currentUser
-                                    }
-                                }}>
-                                    <input type="submit" value={this.state.time} />
-                                </Link>
-                            ) : (
-                                <input className="reservation-input-submit" type="submit" value="Find a Table" onClick={this.handleClick} />
-                                )
-                            }
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+                { this.state.displayTime ? (
+                  <Link to={{
+                    pathname: '/booking/details',
+                      state: {
+                        party_size: this.state.party_size,
+                        date: this.state.date,
+                        time: this.state.time,
+                        restaurant: this.props.restaurant,
+                        currentUser: this.props.currentUser
+                        }
+                      }
+                    }>
+                    <input type="submit" value={this.state.time} />
+                  </Link>
+                    ) : (
+                  <input className="reservation-input-submit" type="submit" value="Find a Table" onClick={this.handleClick} />
+                )}
+              </div>
+            </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ReservationForm;
