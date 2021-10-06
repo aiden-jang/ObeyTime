217c5a002cec69883a4d8c332e246d958a4dbaee
Aiden Chang <skylineciel@gmail.com>
2021-10-06 12:24:54 -0400

    Add reviews routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :show]
     resources :restaurants, only: [:show, :index]
     resources :reservations, only: [:index, :show, :create, :update, :destroy]
+    resources :reviews, only: [:index, :show, :create, :update, :destroy] 
   end
   
   root "static_pages#root"
