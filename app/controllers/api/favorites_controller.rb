c8e784a375232d3101cf7cf9705b090e098187df
Aiden Chang <skylineciel@gmail.com>
2021-10-07 17:09:40 -0400

    Create favorite controller

---
+++ app/controllers/api/favorites_controller.rb
@@ -0,0 +1,36 @@
+class Api::FavoritesController < ApplicationController    
+    before_action :require_logged_in
+
+    def index
+        @favorites = Favorite.all
+    end
+
+    def create
+        @favorite = Favorite.new(favorite_params)
+
+        if @favorite.save!
+            render :show
+        else
+            render json: @favorite.errors.full_messages, status: 422 
+        end
+    end
+
+    def destroy
+        @favorite = Favorite.find(params[:id])
+
+        if @favorite.destroy
+            
+        else
+            render json: ["Invalid"]
+        end
+    end
+
+    private
+
+    def favorite_params
+        params.require(:favorite).permit(
+            :user_id,
+            :restaurant_id,
+        )
+    end
+end
