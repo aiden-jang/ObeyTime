f462ed3a5e298148e877e498e0ac40d4101ddee2
Aiden Chang <skylineciel@gmail.com>
2021-10-09 21:51:34 -0400

    Edit favorite model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -9,7 +9,7 @@
 #  updated_at    :datetime         not null
 #
 class Favorite < ApplicationRecord
-    validates :user_id, :restaurant_id, presence: true
+    validates :user_id, uniqueness: { scope: :restaurant_id }
 
     belongs_to :user,
         foreign_key: :user_id,
