015c631964c0cf8d04b21f75685a28f993941853
Aiden Chang <skylineciel@gmail.com>
2021-10-10 00:27:38 -0400

    Edit reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
+import usersReducer from './users_reducer';
 import restaurantsReducer from './restaurants_reducer';
 import reservationsReducer from './reservations_reducer';
-import usersReducer from './users_reducer';
 import reviewsReducer from './reviews_reducer';
 import favoritesReducer from './favorites_reducer';
 
--- frontend/reducers/filters_reducer.js
+++ frontend/reducers/filters_reducer.js
@@ -11,9 +11,9 @@ const filtersReducer = (state = defaultFilters, action) => {
   Object.freeze(state);
   
   if (action.type === UPDATE_FILTER) {
-      const newFilter = {
-        [action.filter]: action.value
-      };
+    const newFilter = {
+      [action.filter]: action.value
+    };
     return Object.assign({}, state, newFilter);
   } else {
     return state;
