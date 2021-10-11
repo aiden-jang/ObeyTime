99913b189724b944b400f183ae15f310212faeda
Aiden Chang <skylineciel@gmail.com>
2021-10-10 23:41:46 -0400

    Add personal website link

--- frontend/components/main/footer.jsx
+++ frontend/components/main/footer.jsx
@@ -4,6 +4,9 @@ import {
     faLinkedinIn as fabLinkedinIn,
     faGithub as fabGithub
 } from '@fortawesome/free-brands-svg-icons';
+import {
+    faWindowMaximize as farWindowMaximize
+} from '@fortawesome/free-regular-svg-icons';
 
 const Footer = () => {
     return (
@@ -11,6 +14,7 @@ const Footer = () => {
             <div className="footer-container">
                 <a href="https://www.linkedin.com/in/aidenjang/" target="_blank"><FontAwesomeIcon icon={fabLinkedinIn} /></a>
                 <a href="https://github.com/aiden-jang/ObeyTime" target="_blank"><FontAwesomeIcon icon={fabGithub} /></a>
+                <a href="https://aiden-jang.github.io/" target="_blank"><FontAwesomeIcon icon={farWindowMaximize} /></a>
             </div>
         </footer>
     )
